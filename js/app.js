2f583810196d8e8c68f11378de3ae6d827fe72ce
Omar Einea <eineao@gmail.com>
2017-05-10 02:46:17 +0300

    Basic Toolbar with Logo and Login Form

--- js/app.js
+++ js/app.js
@@ -1,16 +1,38 @@
 angular.module('UOSHUB', ['ngMaterial'])
 
-.config(function($locationProvider){
+.config(function($locationProvider, $compileProvider, $mdAriaProvider, $mdThemingProvider){
     $locationProvider.html5Mode({ enabled: true, requireBase: false, rewriteLinks: false });
+    $compileProvider.debugInfoEnabled(false);
+    $mdAriaProvider.disableWarnings();
+    $mdThemingProvider.theme('default')
+        .primaryPalette('green')
+        .accentPalette('blue-grey')
+        .backgroundPalette('blue-grey');
 })
 
 .run(function($rootScope, $window) {
-    ($rootScope.setUrl = function(url) {
-        $rootScope.url = '/static/' + (url || 'index') + '.html';
-    })($window.location.pathname.split('/')[1]);
-    
+    $rootScope.url = filePath($window.location.pathname.split('/')[1]);
     $rootScope.redirect = function(url) {
         $window.history.pushState(null, null, '/' + url);
-        $rootScope.setUrl(url);
-    }
+        $rootScope.url = filePath(url);
+    };
+})
+
+.controller('toolbar', function($scope, $mdDialog) {
+    $scope.login = function(event) {
+        $mdDialog.show({
+            controller: 'toolbar',
+            templateUrl: filePath('login'),
+            parent: angular.element(document.body),
+            targetEvent: event,
+            clickOutsideToClose: true
+        }).then(function(sid) {
+            $scope.sid = sid;
+        }, function(){});
+    };
+    $scope.dialog = $mdDialog;
 });
+
+function filePath(url) {
+    return '/static/' + (url || 'index') + '.html';
+}
