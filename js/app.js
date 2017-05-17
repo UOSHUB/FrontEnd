ee655503eadcce2205d30536279ff1948ad21f18
Omar Einea <eineao@gmail.com>
2017-05-17 08:04:11 +0300

    Implement SideNav Tightening & Better Logic

--- js/app.js
+++ js/app.js
@@ -38,12 +38,13 @@ angular.module('UOSHUB', ['ngMaterial', 'ngRoute', 'ngStorage', 'materialCalenda
         $location.path(link);
     };
     $rootScope.$ls = $localStorage.$default({
-        status: 'logged-out',
+        loggedIn: false,
+        tightNav: false,
         courses: {"1411341":{"ch":3,"crn":10889,"days":["M","W"],"doctor":["Manar Abu Talib","Mtalib@sharjah.ac.ae"],"name":"Web Programming","place":["W8","106"],"section":"11","time":["12:30 PM","1:45 PM"],"color":"red","minutes":[750,825],"points":[[24,75.37037037037038],[62,75.37037037037038]],"length":1.25},"1411440":{"ch":3,"crn":10893,"days":["M","W"],"doctor":["Naveed Ahmed","nahmed@sharjah.ac.ae"],"name":"Introduction to Computer Graphics","place":["W8","005"],"section":"11","time":["8:00 AM","9:15 AM"],"color":"teal","minutes":[480,555],"points":[[24,12.037037037037038],[62,12.037037037037038]],"length":1.25},"1412340":{"ch":3,"crn":10899,"days":["T","R"],"doctor":["Naveed Ahmed","nahmed@sharjah.ac.ae"],"name":"2D/3D Computer Animation","place":["W8","105"],"section":"11","time":["9:30 AM","10:45 AM"],"color":"green","minutes":[570,645],"points":[[43,33.14814814814815],[81,33.14814814814815]],"length":1.25},"1412444":{"ch":3,"crn":10900,"days":["T","R"],"doctor":["Naveed Ahmed","nahmed@sharjah.ac.ae"],"name":"Game Design & Development","place":["W8","106"],"section":"11","time":["11:00 AM","12:15 PM"],"color":"orange","minutes":[660,735],"points":[[43,54.25925925925926],[81,54.25925925925926]],"length":1.25},"0202227":{"ch":3,"crn":10214,"days":["M","W"],"doctor":["Muhieddin AlQaddour","malqaddour@sharjah.ac.ae"],"name":"Critical Reading and Writing","place":["M10","101"],"section":"11","time":["9:30 AM","10:45 AM"],"color":"purple","minutes":[570,645],"points":[[24,33.14814814814815],[62,33.14814814814815]],"length":1.25}}
     });
 })
 
-.controller('Sidenav', function($scope) {
+.controller('Sidenav', function($scope, $rootScope) {
     $scope.links = [{
         title: 'Dashboard',
         icon: 'tachometer'
@@ -60,13 +61,19 @@ angular.module('UOSHUB', ['ngMaterial', 'ngRoute', 'ngStorage', 'materialCalenda
         title: 'Calendar',
         icon: 'globe'
     }];
+    $scope.active = {};
+    $scope.loadPage = function(link) {
+        $scope.active = {};
+        $scope.active[link] = true;
+        $rootScope.redirect(link);
+    };
 })
 
 .controller('Toolbar', function($scope, $mdDialog, $rootScope, $localStorage) {
     $scope.cancel = $mdDialog.cancel;
     $scope.hide = $mdDialog.hide;
     $scope.login = function(event) {
-        $mdDialog.show({
+        /*$mdDialog.show({
             templateUrl: 'login-dialog',
             parent: angular.element(document.body),
             clickOutsideToClose: true,
@@ -74,15 +81,16 @@ angular.module('UOSHUB', ['ngMaterial', 'ngRoute', 'ngStorage', 'materialCalenda
             targetEvent: event,
             scope: $scope
         }).then(function() {
-            $localStorage.status = 'logged-in';
+            $localStorage.loggedIn = true;
             $rootScope.redirect('Dashboard');
         }, function(){
             $localStorage.sid = null;
-        });
+        });*/
+        $localStorage.loggedIn = true;
     };
     $scope.logout = function() {
         $localStorage.sid = null;
-        $localStorage.status = 'logged-out';
+        $localStorage.loggedIn = false;
         $rootScope.redirect('/');
     };
 })
