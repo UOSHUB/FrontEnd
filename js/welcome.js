443be545f327b0db3a7faff6a05ff65720911412
Omar Einea <eineao@gmail.com>
2017-07-18 20:16:09 +0300

    Clean Welcome Page Code & Optimize Its SlideShow

--- js/welcome.js
+++ js/welcome.js
@@ -1,25 +1,8 @@
-app.controller('welcome', function($scope, $interval, $mdDialog, $localStorage) {
-    $scope.slideshow = "img/dashboard.png";
-    $scope.counter = 1;
+app.controller('welcome', function($scope, $interval) {
+    $scope.images = ["dashboard", "schedule", "courses", "email", "calendar"];
+    $scope.counter = 0;
     $interval(function() {
-        $scope.slideshow = "img/" + ["dashboard", "schedule", "courses", "email", "calendar"][$scope.counter] + ".png";
-        $scope.counter = ($scope.counter + 1) % 5;
+        $scope.counter++;
+        $scope.counter %= 5;
     }, 2000);
-    $scope.cancel = $mdDialog.cancel;
-    $scope.hide = $mdDialog.hide;
-    $scope.login = function(event) {
-        $mdDialog.show({
-            templateUrl: 'login-dialog',
-            parent: angular.element(document.body),
-            clickOutsideToClose: true,
-            preserveScope: true,
-            targetEvent: event,
-            scope: $scope
-        }).then(function() {
-            $localStorage.loggedIn = true;
-            $rootScope.redirect('dashboard');
-        }, function(){
-            $localStorage.sid = null;
-        });
-    };
 });
