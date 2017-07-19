6af482767f52e7e50888601262f5d21c554d8466
Omar Einea <eineao@gmail.com>
2017-07-19 23:29:32 +0300

    Remove Dynamic Scripts Loading & Ignore ./CACHE

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,8 @@
-var requested = {}, head = $("head");
-
 function load(file, secure) {
     return {
         requiresLogin: secure,
         templateUrl: file + '.html',
-        controller: file,
-        resolve: {
-            function($http) {
-                if(!requested[file]) {
-                    head.append('<link rel="stylesheet" href="css/' + file + '.css">');
-                    requested[file] = $http.get('js/' + file + '.js')
-                        .then(function(response) {
-                            eval(response.data);
-                        });
-                }
-                return requested[file];
-            }
-        }
+        controller: file
     };
 }
 
