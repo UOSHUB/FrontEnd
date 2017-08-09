bbfd1c2cc6bb9897d3faf47a60a20290ec00a9f0
OmarEinea <eineao@gmail.com>
2017-08-09 12:53:01 +0300

    Place The Login Derivative In A Separate JS File

---
+++ js/login.js
@@ -0,0 +1,31 @@
+app.directive('login', ["$mdDialog", "$http", "$ls", "$goto",
+
+function($mdDialog, $http, $ls, $goto) {
+    return {
+        link: function($scope, element, attrs) {
+            $scope.cancel = $mdDialog.cancel;
+            $scope.submit = $mdDialog.hide;
+            element.on('click', function($event) {
+                $mdDialog.show({
+                    templateUrl: '/static/login.html',
+                    clickOutsideToClose: true,
+                    preserveScope: true,
+                    targetEvent: $event,
+                    parent: $('body'),
+                    scope: $scope
+                }).then(function(data) {
+                    $http.post("/api/login/", data).then(function(response) {
+                        $ls.loggedIn = true;
+                        $goto('dashboard');
+                        if(!$ls.name)
+                            $http.get("/api/details/").then(function(response) {
+                                angular.extend($ls, response.data);
+                            }, function() {});
+                    }, function(response) {
+                        element.triggerHandler('click');
+                    });
+                }, function() {});
+            });
+        }
+    }
+}])
