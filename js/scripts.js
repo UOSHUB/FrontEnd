7d80e6b39583c28678fc9879f0c60165b39df0c4
Omar Einea <eineao@gmail.com>
2017-08-17 18:10:04 +0300

    Add A Function To Structure Course Data In A Printable Form

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,24 @@ function currentTerm() {
     var date = new Date(), month = date.getMonth() + 1;
     return date.getFullYear() + (month > 7 ? '10' : month < 6 ? '20' : '30');
 }
+
+function structureCourse(course, id) {
+    return {
+        name: course.name,
+        columns: [
+            [
+                ['Doctor Name', course.doctor[0], 'user'],
+                ['Days', course.days.join(' '), 'calendar-o'],
+                ['Start Time', course.time[0], 'hourglass-start'],
+                ['Section', course.section, 'puzzle-piece', '(Section)'],
+                ['Course key', id, 'key']
+            ], [
+                ['Doctor Email', course.doctor[1], 'envelope'],
+                ['Building, Room', course.place.join(', '), 'map-marker'],
+                ['End Time', course.time[1], 'hourglass-end'],
+                ['Credit Hours', course.ch, 'certificate', '(CrHrs)'],
+                ['CRN', course.crn, 'info']
+            ]
+        ]
+    };
+}
