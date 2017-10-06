3e63fbfe66c24eb6e0a882692225bb5b5c9066bc
Omar Einea <eineao@gmail.com>
2017-10-06 19:29:06 +0400

    Clean Up Some More Hardcoded Data And Change Colors & Padding

--- js/dashboard.js
+++ js/dashboard.js
@@ -35,25 +35,6 @@ function($scope, $ls, $http) {
         ).toUpperCase();
     };
 
-    $scope.repeat = [0,1,2,3,4,5,6,7,8,9];
-
-    $scope.tasks = [{
-        title: 'Project Final Submission Due',
-        group: 'Introduction to Computer Graphics',
-        day: 'Today',
-        time: '11:59 AM'
-    }, {
-        title: 'Chapter 6 Available',
-        group: 'Critical Reading and Writing',
-        day: 'Tomorrow',
-        time: '1:59 AM'
-    }, {
-        title: 'Notes - 3dsmax Available',
-        group: '2D/3D Computer Animation',
-        day: 'In Two days',
-        time: '5:59 AM'
-    }];
-
     $scope.grades = [{
         course: 'Networking Fundamentals',
         letter: 'A',
