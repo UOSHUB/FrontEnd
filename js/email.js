7f64829f1f773b9281217f43fe3038b78a57ef22
Omar Einea <eineao@gmail.com>
2017-10-16 00:29:13 +0400

    Display Email's Body In Big Card & Save Opened Email & Clean Up

--- js/email.js
+++ js/email.js
@@ -1,35 +1,27 @@
 app.controller('email', ["$scope", "$ls", "$http",
 
 function($scope, $ls, $http) {
+    $scope.tabs = ["personal", "courses", "events"];
+
+    if(!$ls.selected.tab)
+        $ls.selected.tab = 0;
+
     ($scope.getEmails = function(category) {
         $http.get("/api/emails/" + category + "/").then(function(response) {
             var emails = {};
             emails[category] = response.data;
             angular.extend($ls.emails, emails);
         }, error);
-    })();
+    })($scope.tabs[$ls.selected.tab]);
 
-    $scope.tabs = ["personal", "courses", "events"];
-    $scope.filter = 0;
-    $scope.repeat = new Array(15);
-    $scope.content = [{
-        initial: 'D',
-        title: 'Personal Email #',
-        sender: function(index) { return 'Doctor Name #' + index + ' <doctor' + index + '@sharjah.ac.ae>'; },
-        time: ' Days Ago'
-    }, {
-        initial: 'C',
-        title: 'Course Announcement #',
-        sender: function(index) { return 'Course Name #' + index; },
-        time: ' Hours Ago'
-    }, {
-        initial: 'S',
-        title: 'New Announcement #',
-        sender: function(index) { return 'Sender Name #' + index + ' <doctor' + index + '@sharjah.ac.ae>'; },
-        time: ' Minutes Ago'
-    }];
-    ($scope.openEmail = function(index) {
-        $scope.current = $scope.content[$scope.filter];
-        $scope.current.index = index + 1;
-    })(0);
+    $scope.openEmail = function(tab, index) {
+        $ls.selected.email = [tab, index];
+    };
+
+    $scope.isSelected = function(tab, index) {
+        return $ls.selected.email[0] == tab && $ls.selected.email[1] == index;
+    };
+
+    if(!$ls.selected.email)
+        $scope.openEmail("personal", 0);
 }]);
