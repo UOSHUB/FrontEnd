f0f76848d68c2a5fdb17c85491f99fb66ad6e85a
Omar Einea <eineao@gmail.com>
2017-11-27 18:21:25 +0400

    Add A Directive With Full Logic For Deadlines Submission

---
+++ js/submit.js
@@ -0,0 +1,27 @@
+app.directive("submit", ["$http", "$timeout", "$toast",
+
+function ($http, $timeout, $toast) {
+    return {
+        link: function ($scope, element, attr) {
+            $timeout(function() {
+                var input = element.find("input");
+                input.bind("change", function() {
+                    var files = new FormData();
+                    angular.forEach(input[0].files, function(file, index) {
+                        files.append("file" + index, file);
+                    });
+                    $http.post("/api/submit/" + attr.submit, files, {
+                        headers: {"Content-Type": undefined}
+                    }).then(function (response) {
+                        $toast("Submitted files successfully");
+                    }, function(e) {
+                        $toast("Faild to submit files!");
+                    });
+                });
+                element.on("click", function() {
+                    input[0].click();
+                });
+            });
+        }
+    };
+}]);
