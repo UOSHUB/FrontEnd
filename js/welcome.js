dd3998cd9501e35364299d8b94ae41fca3c017b6
Omar Einea <eineao@gmail.com>
2018-11-06 15:18:23 +0400

    Added A Demo Button To The Welcome Page To Start The Demo

--- js/welcome.js
+++ js/welcome.js
@@ -1,9 +1,22 @@
-app.controller("welcome", ["$scope", "$interval",
+app.controller("welcome", ["$scope", "$interval", "$http", "$ls", "$goto",
 
-function($scope, $interval) {
+function($scope, $interval, $http, $ls, $goto) {
     $scope.counter = 0;
     $interval(function() {
         $scope.counter++;
         $scope.counter %= 5;
     }, 2000);
+
+    $scope.startDemo = function() {
+        $http.get("/api/demo/").then(function(response) {
+            $ls.$default(angular.merge({
+                session: { version: version },
+                selected: {
+                    term: term,
+                    email: [],
+                    tab: 0
+                }}, response.data));
+            $goto("dashboard");
+        });
+    };
 }]);
