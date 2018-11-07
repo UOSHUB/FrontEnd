e5bd2fca025104de041f6ad83a533cfde95441fc
Omar Einea <eineao@gmail.com>
2018-11-07 12:32:39 +0400

    Updated All Welcome Page Slideshow Images & Added One For Reports

--- js/welcome.js
+++ js/welcome.js
@@ -4,13 +4,14 @@ function($scope, $interval, $http, $ls, $goto) {
     $scope.counter = 0;
     $interval(function() {
         $scope.counter++;
-        $scope.counter %= 5;
+        $scope.counter %= 6;
     }, 2000);
 
     $scope.startDemo = function() {
         $http.get("/api/demo/").then(function(response) {
             $ls.$default(angular.merge({
                 session: { version: version },
+                emails: { action: "select" },
                 selected: {
                     term: term,
                     email: [],
